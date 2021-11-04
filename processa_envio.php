1483e2e90cc991f9e388653474f42d4f446a295c
Vitor <vitomartinsalmeida@gmail.com>
2021-11-04 11:33:46 -0300

    adicionando protocolo google email e senha

--- processa_envio.php
+++ processa_envio.php
@@ -62,19 +62,18 @@
 	    //Server settings
 	    $mail->SMTPDebug = 2;                      //Enable verbose debug output
 	    $mail->isSMTP();                                            //Send using SMTP
-	    $mail->Host       = 'smtp.example.com;smtp2.example.com';                     //Set the SMTP server to send through
+	    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
 	    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-	    $mail->Username   = 'user@example.com';                     //SMTP username
-	    $mail->Password   = 'secret';                               //SMTP password
+	    $mail->Username   = 'alcachofrafrederico@gmail.com';                     //SMTP username
+	    $mail->Password   = 'Alcachofra123';                               //SMTP password
 	    $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
 	    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
 	    //Recipients
-	    $mail->setFrom('from@example.com', 'Mailer');
-	    $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
-	    $mail->addAddress('ellen@example.com');               //Name is optional
+	    $mail->setFrom('alcachofrafrederico@gmail.com', 'alcachofra remetente');
+	    $mail->addAddress('alcachofrafrederico@gmail.com', 'alcachofra destinatário');     //Add a recipient
 	    $mail->addReplyTo('info@example.com', 'Information');
-	    $mail->addCC('cc@example.com');
+	    //$mail->addCC('cc@example.com');
 	    $mail->addBCC('bcc@example.com');
 
 	    //Attachments
