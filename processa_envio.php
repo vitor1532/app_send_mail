046869558ce57e23478cdef4e384254b1367da86
Vitor <vitomartinsalmeida@gmail.com>
2021-11-05 11:52:26 -0300

    ainda tá dando errado

--- processa_envio.php
+++ processa_envio.php
@@ -64,6 +64,7 @@
 	    $mail->Password   = 'Alcachofra123';                               //SMTP password
 	    $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
 	    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+	    
 
 	    //Recipients
 	    $mail->setFrom('alcachofrafrederico@gmail.com', 'alcachofra remetente');
